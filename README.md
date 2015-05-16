c56da9234a091ef7d81e0ff535cb4b43ded12c6f
GN Trifanoff <aznaum@gmail.com>
2015-05-16 12:23:15 -0700

    changed README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # octoberjoy
+
+## Universal Baseball Association implementation
--- public/script/namepool.js
+++ public/script/namepool.js
@@ -3,16 +3,11 @@ var NAMEPOOL = function() {
   var LISTLEN = SYLLABLES.length;
 
   draw = function() {
-    var x, y1, y2, y3;
+    var x, y1, y2;
     x = Math.floor(Math.random() * 2);
     y1 = Math.floor(Math.random() * LISTLEN);
     y2 = Math.floor(Math.random() * LISTLEN);
-    if (x == 0) {
-        return SYLLABLES[y1] + SYLLABLES[y2];
-    } else {
-        y3 = Math.floor(Math.random() * LISTLEN);
-        return SYLLABLES[y1] + SYLLABLES[y2] + SYLLABLES[y3];
-    }
+    return SYLLABLES[y1] + SYLLABLES[y2];
   };
     
   return {
